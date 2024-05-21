112f80daecc1c9f3dd2be862e37e1bbc32f394c2
Clint <cwolf12@cnm.edu>
2024-05-21 09:55:08 -0600

    After PATTERN work

--- src/main/java/edu/cnm/deepdive/Mode.java
+++ src/main/java/edu/cnm/deepdive/Mode.java
@@ -1,6 +1,10 @@
 package edu.cnm.deepdive;
 
+import java.util.regex.Pattern;
+
 public enum Mode
 {
-    CF, FC, CK, KC, KF, FK
+    CF, FC, CK, KC, KF, FK;
+
+    public static final Pattern PATTERN = Pattern.compile("CF|FC|CK|KC|KF|FK",Pattern.CASE_INSENSITIVE);
 }
--- src/main/java/edu/cnm/deepdive/TemperatureConverter.java
+++ src/main/java/edu/cnm/deepdive/TemperatureConverter.java
@@ -1,22 +1,54 @@
 package edu.cnm.deepdive;
 
+import java.util.Scanner;
+
 public class TemperatureConverter
 {
 
   private static final int CELSIUS_FAHRENHEIT_OFFSET = 32;
   private static final double KELVIN_CELSIUS_OFFSET = 273.15;
   public static final double CELSIUS_FAHRENHEIT_SCALE_FACTOR = 1.8;
-  private static Mode var_mode = Mode.CF;
+  private static Mode mmode = Mode.CF;
 
   public static void main(String[] args)
   {
-    System.out.printf("Starting mode = %s%n", var_mode);
+    System.out.printf("Starting mode = %s%n", mmode);
+
+    // while there is input
+    // - check if input matches one of the modes;
+    //    if so, change mode
+    // otherwise check if input is a number
+    //    if so, perform conversion
+    // otherwise, halt.
+
+    Scanner var_scanner = new Scanner(System.in);
+    while (var_scanner.hasNext())
+    {
+      if (var_scanner.hasNext(Mode.PATTERN))
+      {
+        mmode = Mode.valueOf(var_scanner.next(Mode.PATTERN).toUpperCase());
+        System.out.printf("Change mode to = %s%n", mmode);
+      }
+      else if (var_scanner.hasNextDouble())
+        {
+          double value = var_scanner.nextDouble();
+          double convertedValue = convert(value);
+          System.out.printf("Input = %.2f; output = %.2f%n", value, convertedValue);
+        }
+        else
+          {
+            // throw an error
+            throw new IllegalArgumentException(String.format("Invalid input: %s%n", var_scanner.next()));
+          }
+    }
+
+
     for (String arg : args)
     {
       try
       {
-        var_mode = Mode.valueOf(arg.toUpperCase());
-        System.out.printf("Change mode to = %s%n", var_mode);
+        mmode = Mode.valueOf(arg.toUpperCase());
+        System.out.printf("Change mode to = %s%n", mmode);
       }
       catch (IllegalArgumentException e)
       {
@@ -31,7 +63,7 @@ public class TemperatureConverter
   private static double convert(double value)
   {
     //throw new UnsupportedOperationException();
-    return switch (var_mode)
+    return switch (mmode)
     {
       case CF -> value * CELSIUS_FAHRENHEIT_SCALE_FACTOR + CELSIUS_FAHRENHEIT_OFFSET;
       case CK -> value + KELVIN_CELSIUS_OFFSET;
